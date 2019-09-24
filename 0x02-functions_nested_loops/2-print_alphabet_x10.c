7208aee21fffab7395cdb2e6e4fdd71177387396
Sebastian Ruiz <1203@holbertonschool.com>
2019-09-24 03:05:06 +0000

    README.md

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	for(i = 1 ; i <= 10 ; i++)
+	{
+		for(c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+		return (1);
+
+	return (0);
+}
