cddd9b2faaadf2e4f88506a4803c35f0f2730296
Sebastian Ruiz <1203@holbertonschool.com>
2019-09-24 21:08:18 +0000

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _isalpha - Checks for alphabetic character.
+ * @c: The character to print.
+ * Return: Returns 1 if is a letter lowercase or uppercase, 0 otherwise.
  */
 int _isalpha(int c)
 {
-	if
-
-	return (0);
+	if ((c > 64 && c < 91) || (c > 96 && c < 123))
+	return(1);
+	else
+return (0);
 }
