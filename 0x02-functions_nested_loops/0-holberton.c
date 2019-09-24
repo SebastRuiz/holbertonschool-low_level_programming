af06174f4917eef1fa69b4664a2210143438fe8f
Sebastian Ruiz <1203@holbertonschool.com>
2019-09-24 14:31:33 +0000

    0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,20 +1,20 @@
 #include "holberton.h"
-
 /**
- *_putchar
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
-int main (void)
+int main(void)
 {
 	_putchar('H');
-        _putchar('o');
-        _putchar ('l');
-        _putchar ('b');
-        _putchar ('e');
-        _putchar ('r');
-        _putchar ('t');
-        _putchar ('o');
-        _putchar ('n');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 
 	_putchar ('\n');
 
