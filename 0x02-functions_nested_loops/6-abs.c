33afece9b086ceab9c390ee0b68182e4dd827806
Sebastian Ruiz <1203@holbertonschool.com>
2019-09-24 22:45:28 +0000

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,17 +1,19 @@
 #include "holberton.h"
 /**
  * _abs - Computes the absolute value of an integer.
- * @c: The character to print.
+ * @n: The character to print.
  * Return: 0
  */
 int _abs(int n)
 {
-	int c;
-	if (n > 0){
-	c = n;
-	}
-	else {
-        c = n * -1;
-	}
-        return (c);
+int c;
+if (n > 0)
+{
+c = n;
+}
+else
+{
+c = n * -1;
+}
+return (c);
 }
