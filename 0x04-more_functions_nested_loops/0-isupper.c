1d8b80c52ee054e73d9f0374c9560c36618bab78
Sebastian Ruiz <1203@holbertonschool.com>
2019-09-26 17:47:12 +0000

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isupper - Checks for uppercase character.
+ * @c: The character to print.
+ * Return: Returns 1 if is uppercase, returns 0 otherwise.
+ */
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+        else
+	{
+		return (0);
+	}
+}
