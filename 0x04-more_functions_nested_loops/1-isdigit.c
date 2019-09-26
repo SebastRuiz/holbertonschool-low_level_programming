45bb9036df9d7ef0c2c5890164991246f9a281f2
Sebastian Ruiz <1203@holbertonschool.com>
2019-09-26 19:11:10 +0000

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isdigit - Checks for a digit (0 through 9).
+ * @c: The character to print.
+ * Return: Returns 1 if is a digit, returns 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
